f7f78243c1742baf5f3f07452b156b7ff69fbf07
zeimedee <42755623+zeimedee@users.noreply.github.com>
2024-01-08 08:08:39 +0000

    first soap server

---
+++ server.js
@@ -0,0 +1,42 @@
+const soap = require('soap')
+const http = require('http')
+
+//define service 
+
+const service = {
+    WeatherService : {
+        WeatherPort :{
+            GetWeather: function(args, callback){
+                //perform logic to get weather based on location
+                const location = args.location;
+                const temperature = '25°C';
+                const description = 'sunny';
+
+                //return response
+                const result = {
+                    temperature:temperature,
+                    description:description
+                };
+                callback(null, result)
+            }
+        }
+    }
+};
+
+// create soap server
+
+const wsdl = require('fs').readFileSync('./weatherService.wsdl', 'utf8');
+// const server = soap.listen({ path:'/weather', xml:wsdl}, function(){
+//     console.log('SOAP server running on http://localhost:8080/weather?wsdl' )
+// });
+
+// //attach service implementation to server
+// server.addService(xml, service, {suppressStack:true});
+
+var server = http.createServer(function(request,response) {
+    response.end('404: Not Found: ' + request.url);
+});
+server.listen(8080)
+soap.listen(server, '/weather', service, wsdl, function(){
+    console.log('server initialized');
+  });
